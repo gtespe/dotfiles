e8c5f3c19e7c85cba16e434a91d6f898ea28870c
gtespe <espegrant@gmail.com>
2016-09-14 13:37:39 -0400

    added ycm to vim addon getter

--- .vim/bundle/getaddons.sh
+++ .vim/bundle/getaddons.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-git clone https://github.com/scrooloose/syntastic.git && git clone https://github.com/scrooloose/nerdtree.git && git clone https://github.com/ctrlpvim/ctrlp.vim.git
+git clone https://github.com/scrooloose/syntastic.git && git clone https://github.com/scrooloose/nerdtree.git && git clone https://github.com/ctrlpvim/ctrlp.vim.git && https://github.com/Valloric/YouCompleteMe#ubuntu-linux-x64 && ./install.sh
+
 
